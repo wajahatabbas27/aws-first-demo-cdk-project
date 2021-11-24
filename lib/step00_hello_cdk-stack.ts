a60d255b5683ee5d9731fb4164b2be3a8279c1a7
wajahatabbas27 <wajahatabbas27@gmail.com>
2021-11-24 12:16:05 +0500

    created first cloud formaton and also created the s3 instance

--- lib/step00_hello_cdk-stack.ts
+++ lib/step00_hello_cdk-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
+import * as s3 from '@aws-cdk/aws-s3';
 
 export class Step00HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step00HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // L2 Bucket
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    })
   }
 }
